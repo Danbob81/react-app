07cfd90884898526bc54a0035147609348925f87
Daniel Higginson <danbobhig@gmail.com>
2023-02-16 16:24:49 +0000

    add add EventsClass, EventsFunctional and EventsBinding components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,18 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import EventBinding from './components/EventBinding';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" /> */}
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component!" name="Mike" />
+      {/* <StatefulGreetingWithPrevState greeting="I'm a stateful class component!" name="Mike" /> */}
+      {/* <EventsFunctional /> */}
+      {/* <EventsClass /> */}
+      <EventBinding />
     </div>
   );
 }
